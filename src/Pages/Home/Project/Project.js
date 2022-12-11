99beee8312b4ba76e5ad28d26cd82d9a9e5b92d5
SyedaAishaAhmad <18aishaahmad@gmail.com>
2022-12-11 18:28:50 +0600

    project section

---
+++ src/Pages/Home/Project/Project.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import project1 from '../../../Assets/Screenshot (397).png'
+import project2 from '../../../Assets/Screenshot (398).png'
+import project3 from '../../../Assets/Screenshot (399).png'
+
+const Project = () => {
+    return (
+        <div className='w-10/12 mx-auto px-5 my-32'>
+            <h1 className='text-5xl text-primary font-bold text-center'>My Projects</h1>
+            <div className='grid grid-cols-3 mt-10'>
+                <div className=" card w-11/12 mx-auto mt-10 bg-base-100 shadow-xl">
+                    <figure><img src={project1} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className="card-title w-50 mx-auto text-center">
+                            B-picker
+                            <a href="https://b-picker-e010a.web.app/"><div className="badge badge-primary">Live Site</div></a>
+                        </h2>
+                        <p>A reseller bookshop where user can buy or sell books.</p>
+                    </div>
+                </div>
+
+                <div className=" card w-11/12 mx-auto mt-10 bg-base-100 shadow-xl">
+                    <figure><img src={project2} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className="card-title w-50 mx-auto text-center">
+                            Excursion
+                            <a href="https://excursion-e221c.web.app/"><div className="badge badge-primary">Live Site</div></a>
+                        </h2>
+                        <p>Travel service Website.</p>
+                    </div>
+                </div>
+
+                <div className=" card w-11/12 mx-auto mt-10 bg-base-100 shadow-xl">
+                    <figure><img src={project3} alt="Shoes" /></figure>
+                    <div className="card-body text-center">
+                        <h2 className="card-title w-50 mx-auto text-center">
+                            Learning Tech
+                            <a href="https://rise-turn-club.web.app/"><div className="badge badge-primary">Live Site</div></a>
+                        </h2>
+                        <p>Learning Platform.</p>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Project;
